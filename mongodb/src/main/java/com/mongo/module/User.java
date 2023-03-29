798225521703c6b5263b75e0a576d9636391e7c1
jikai123-do <jikai8268@qq.com>
2023-03-29 15:37:08 +0800

    前后端通讯

---
+++ mongodb/src/main/java/com/mongo/module/User.java
@@ -0,0 +1,37 @@
+package com.mongo.module;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by x on 2023/3/29.
+ */
+@Repository
+public class User {
+    public String name;
+    public  Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
